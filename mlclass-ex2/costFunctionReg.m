32348373a7b62f45c0138b4b67645b79f8f09644
Leo Law <lawmauwa@gmail.com>
2014-07-17 21:43:09 -0400

    RegularizedFunction

--- mlclass-ex2/costFunctionReg.m
+++ mlclass-ex2/costFunctionReg.m
@@ -17,7 +17,9 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+J = (1/m) * sum(-y .* log(sigmoid((theta' * X'))') - ((1 .- y) .* log(1 .- sigmoid((theta' * X'))'))) + (lambda/(2*m)) * (sum( theta(2:end) .^2 ))
 
+grad = ((((sigmoid((theta' * X')') .- y)') * X ) .* (1/m))' + [0; (lambda .* theta(2:end)/m)]
 
 
 
